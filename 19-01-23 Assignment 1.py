2c80a2ee31bffeef888be6ba4ab395146926f65b
SaiGanesh9999 <110873642+SaiGanesh9999@users.noreply.github.com>
2023-01-20 08:40:35 +0530

    Create 19-01-23 Assignment 1.py

---
+++ 19-01-23 Assignment 1.py
@@ -0,0 +1,33 @@
+import math
+import os
+import random
+import re
+import sys
+
+def isBalanced(s):
+    # Write your code here
+    while(s):
+        #print(s)
+        if '{}' in s:
+            s = s.replace('{}','')
+        elif '()' in s:
+            s = s.replace('()','')
+        elif '[]' in s:
+            s = s.replace('[]','')
+        else:
+            return "NO"
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        s = input()
+
+        result = isBalanced(s)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
